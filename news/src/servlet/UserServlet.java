d8809ed74398fb8351610e1ca154babf37fa54a7
liuchong <970009721@qq.com>
2019-04-29 17:36:48 +0800

    xx

---
+++ news/src/servlet/UserServlet.java
@@ -0,0 +1,10 @@
+package servlet;
+
+import java.io.IOException;
+
+public class UserServlet extends javax.servlet.http.HttpServlet {
+    @Override
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
